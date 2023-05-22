db6e77c6c16a2b820a78f826e8a3a2a9fef2cc31
Rain <89419093+1rainwall@users.noreply.github.com>
2023-05-21 22:18:54 -0500

    Database Management

---
+++ src/listeners/client/guildCreate.ts
@@ -0,0 +1,15 @@
+import { Listener } from "@sapphire/framework";
+import { Guild } from "discord.js";
+import { checkVoiceMembers } from "../../functions/checkVoice";
+
+export class GuildCreateListener extends Listener {
+  public constructor(context: Listener.Context, options: Listener.Options) {
+    super(context, {
+      ...options,
+    });
+  }
+  public async run(guild: Guild) {
+    await checkVoiceMembers();
+    this.container.logger.info(`El bot se ha unido a ${guild.name}(${guild.id}.`);
+  }
+}
